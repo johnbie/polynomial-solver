2af28cef63383673cc76372d00be8b3a30d9020f
johnbie <johnqwerty.lee1@gmail.com>
2022-10-15 10:07:10 -0700

    Fixing bug (WIP)

--- src/test/model/PolynomialTest.java
+++ src/test/model/PolynomialTest.java
@@ -1,9 +1,6 @@
 package model;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PolynomialTest {
