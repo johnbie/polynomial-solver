19b99536cbbc1c9a15b9cd21da1feca0185aa3b9
johnbie <johnqwerty.lee1@gmail.com>
2022-11-28 14:46:34 -0800

    Logs now show up on console after application closes.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,12 +1,29 @@
 package ui;
 
+import model.Event;
+import model.EventLog;
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 /*
  * The starting class for application.
  */
 public class Main {
     // EFFECTS: starts app
     public static void main(String[] args) {
-        new PolynomialGuiApp();
         //new PolynomialTerminalApp();
+        JFrame frame = new PolynomialGuiApp();
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                EventLog eventLog = EventLog.getInstance();
+                for (Event next : eventLog) {
+                    System.out.println(next.toString());
+                }
+            }
+        });
+
     }
 }
